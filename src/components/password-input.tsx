0f8854dc08cd4b564de13b84545106b769979a4d
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-20 21:19:12 +0300

    changing icon to be lucide react icons and done with the auth logic

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -1,6 +1,6 @@
 import { Input, InputProps } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
+import { Eye, EyeOff } from "lucide-react";
 import React, { useState } from "react";
 const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -20,9 +20,9 @@ const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
           className="absolute right-3 top-1/2 -translate-y-1/2 transform text-muted-foreground"
         >
           {showPassword ? (
-            <EyeClosedIcon className="size-5" />
+            <EyeOff className="size-5" />
           ) : (
-            <EyeOpenIcon className="size-5" />
+            <Eye className="size-5" />
           )}
         </button>
       </div>
