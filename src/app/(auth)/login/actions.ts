2df6755fb4fd9cad44a70200433ca71c6465957f
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-20 20:05:52 +0300

    setup server actions logic for auth

---
+++ src/app/(auth)/login/actions.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { loginSchema, LoginValues } from "@/lib/validation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { verify } from "@node-rs/argon2";
+import { lucia } from "@/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export const login = async (
+  credentials: LoginValues,
+): Promise<{ error: string }> => {
+  try {
+    const { username, password } = loginSchema.parse(credentials);
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+        },
+      },
+    });
+    if (!existingUser || !existingUser.passwordHash) {
+      return {
+        error: "Incorrect username or password ",
+      };
+    }
+    const vaildPassword = await verify(existingUser.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+    if (!vaildPassword) {
+      return {
+        error: "Incorrect username or password ",
+      };
+    }
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Somethign went wrong. Please try again.",
+    };
+  }
+};
---
+++ src/lib/validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+const requiredString = z.string().trim().min(1, "Required");
+//Schemas
+export const signUpSchema = z.object({
+  email: requiredString.email("Invaild email address"),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]+$/,
+    "Only letters, - and _ allowed",
+  ),
+  password: requiredString.min(8, "Must be at least 8 characters"),
+});
+
+export const loginSchema = z.object({
+  username: requiredString,
+  password: requiredString,
+});
+//Types
+export type SignUpValues = z.infer<typeof signUpSchema>;
+export type LoginValues = z.infer<typeof loginSchema>;
