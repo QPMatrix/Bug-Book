89e78a129f02eac4284ac5cb98a1269027c9cf3c
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-23 20:47:25 +0300

    updating the react query omptimise and functions adding loading and not found

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -6,6 +6,7 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import React, { cache } from "react";
 import UserProfile from "./components/user-profile";
+import UserPostFeed from "./components/user-posts-feed";
 interface PageProps {
   params: { username: string };
 }
@@ -45,6 +46,12 @@ const Page = async ({ params: { username } }: PageProps) => {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <UserProfile user={user} loggedInUserId={loggedInUser.id} />
+        <div className="rounded-2xl bg-card p-5 shadow-sm">
+          <h2 className="text-center text-2xl font-bold">
+            {user.displayName}&apos;s posts
+          </h2>
+        </div>
+        <UserPostFeed userId={user.id} />
       </div>
       <TrendsSidebar />
     </main>
