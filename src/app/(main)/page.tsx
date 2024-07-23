370546de58d17aa699c59ce6e21aed546657ce8e
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-23 09:27:08 +0300

    making posts ui to showing then

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,21 @@
 import PostEditor from "@/components/posts/editor/post-editor";
+import Post from "@/components/posts/post";
+import prisma from "@/lib/prisma";
+import { PostDataInclude } from "@/lib/types";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: PostDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
   return (
-    <main className="w-full">
-      <div className="w-full">
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDate, formatDistanceToNowStrict } from "date-fns";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatRelativeDate = (from: Date) => {
+  const cureentDate = new Date();
+  if (cureentDate.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(from, { addSuffix: true });
+  } else {
+    if (cureentDate.getFullYear() === from.getFullYear()) {
+      return formatDate(from, "d MMM");
+    } else {
+      return formatDate(from, "d MMM yyyy");
+    }
+  }
+};
