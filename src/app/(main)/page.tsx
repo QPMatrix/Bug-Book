2811521b096b28018680bca02970424dfaee1bfe
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-23 16:30:18 +0300

    adding following feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,26 @@
 import ForYouFeed from "@/components/posts/for-you-feed";
 import PostEditor from "@/components/posts/editor/post-editor";
 import TrendsSidebar from "@/components/trends-sidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "@/components/posts/following-feed";
 
 export default async function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
---
+++ src/components/posts/following-feed.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { PostData, PostsPage } from "@/lib/types";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+import React from "react";
+import Post from "@/components/posts/post";
+import KyInstance from "@/lib/ky";
+import { Button } from "../ui/button";
+import InfiniteScrollContainer from "../infinite-scroll-container";
+import PostsSkeleton from "./posts-skeleton";
+import DeletePostDialog from "./delete-dialog";
+
+const FollowingFeed = () => {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "following"],
+    queryFn: ({ pageParam }) =>
+      KyInstance.get(
+        "/api/posts/following",
+        pageParam ? { searchParams: { cursor: pageParam } } : {},
+      ).json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+  if (status === "pending") {
+    return <PostsSkeleton />;
+  }
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-destructive">
+        No post found. Start follwing pepole to see their posts
+      </p>
+    );
+  }
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        An error occured while lodaing posts.
+      </p>
+    );
+  }
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+};
+
+export default FollowingFeed;
